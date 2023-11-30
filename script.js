ac255990242658668eb036b7dc703db213f05efc
harsh007-github <khandelwalharsh401@gmail.com>
2023-11-30 10:12:07 +0530

    Record and Capture button changes made

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let video = document.querySelector("video");
 
+// Permissions needed to entered and with their rquirements (true in this case)
 let constraints = {
     audio:true,
     video:true
 }
 
-navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-    video.srcObject = stream;
-})
+// navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+//     video.srcObject = stream;
+// })
