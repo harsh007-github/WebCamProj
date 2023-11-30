e439c7d22e31d8a95a26558b8d31807fe1ac25ec
harsh007-github <khandelwalharsh401@gmail.com>
2023-11-30 09:17:37 +0530

    Files added

---
+++ script.js
@@ -0,0 +1,10 @@
+let video = document.querySelector("video");
+
+let constraints = {
+    audio:true,
+    video:true
+}
+
+navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    video.srcObject = stream;
+})
