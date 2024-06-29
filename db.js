30d12f6d32134b3ec3105c2c251500626281d8c9
harsh007-github <khandelwalharsh401@gmail.com>
2024-06-29 09:22:09 +0530

    DB Changes made

--- db.js
+++ db.js
@@ -1,17 +1,20 @@
 // Open Database
 // Create objectStore(just like tables)
 let db;
-let request = indexedDB.open("fileStorage");  //Opening DB with a certain name
-request.onerror = (e)=> {
-    console.log("DB Error");
-    db = request.result; 
+let request = indexedDB.open("fileStorage"); //Opening DB with a certain name
+//If Opening DB has any errors
+request.onerror = (e) => {
+  console.log("DB Error");
+  db = request.result;
 };
+//If Opening DB is successful
 request.onsuccess = (e) => {
-    console.log("DB Opened Successfully");
+  console.log("DB Opened Successfully");
 };
-request.onupgradeneeded = (e)=> {
-    console.log("DB Upgraded and intial DB created");
-    db = request.result;
-    db.createObjectStore("video", { keyPath: "id" });
-    db.createObjectStore("image", { keyPath: "id" });
+//If Opening DB is upgraded
+request.onupgradeneeded = (e) => {
+  console.log("DB Upgraded and intial DB created");
+  db = request.result;
+  db.createObjectStore("video", { keyPath: "id" });
+  db.createObjectStore("image", { keyPath: "id" });
 };
